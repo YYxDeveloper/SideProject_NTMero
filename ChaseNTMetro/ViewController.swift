115b641d8f968606009452ab93f133a30613fcbf
young <yyxdev@gmail.com>
2021-03-01 14:40:54 +0800

    Initial Commit

---
+++ ChaseNTMetro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChaseNTMetro
+//
+//  Created by 呂子揚 on 2021/3/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
